c2941380629322c84e10e43583bd0e21ef3a64d5
JulianBoi <boi.julian@yahoo.com>
2017-06-03 10:04:06 +0200

    Création des variables avec les bibliothèques (Sans fichier JSON)

---
+++ app.js
@@ -0,0 +1,6 @@
+//Création des variables utile à l'application
+var app = require('express')(),
+    server = require('http').createServer(app),
+    io = require('socket.io').listen(server),
+    ent = require('ent'),
+    fs = require('fs');
